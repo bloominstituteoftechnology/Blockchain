f671ae6bd885b311f1ffa6182f6e81576f9abcb0
chasegarsee <chasegarseesoftwaredevelopment@gmail.com>
2019-08-07 17:44:29 -0500

    Day 1 MVP

--- client_mining_p/miner.py
+++ client_mining_p/miner.py
@@ -15,8 +15,31 @@ if __name__ == '__main__':
         node = "http://localhost:5000"
 
     coins_mined = 0
+
+    def valid_proof(last_proof, proof):
+        """
+        Validates the Proof:  Does hash(last_proof, proof) contain 4
+        leading zeroes?
+        """
+        guess = f'{last_proof}{proof}'.encode()
+        guess_hash = hashlib.sha256(guess).hexdigest()
+        return guess_hash[:6] == "000000"
+
+
     # Run forever until interrupted
     while True:
+        r = requests.get(url = node + "/last_proof")
+        data = r.json()
+
+        proof = 0
+        while (valid_proof(data['proof'], proof) is False):
+            proof += 1
+        post_data = {
+            'proof': guess_hash,
+
+        }
+        r = requests.post(url=node + "/mine", json=post_data)
+        data = r.json()
         # TODO: Get the last proof from the server and look for a new one
         # TODO: When found, POST it to the server {"proof": new_proof}
         # TODO: We're going to have to research how to do a POST in Python
