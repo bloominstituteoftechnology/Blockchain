dd580b02360a2b59ac549779405ab1fe4596e990
Freekeyboy <jjhutton81@gmail.com>
2019-08-07 20:53:28 -0700

    Started miner.py.

--- client_mining_p/README.md
+++ client_mining_p/README.md
@@ -9,8 +9,8 @@ Furthermore, the amount of work needed to actually mine a block is a bit low.  W
 
 *Server*
 Modify the server we created to:
-* Remove the `proof_of_work` function from the server.
-* Change `valid_proof` to require *6* leading zeroes.
+*X Remove the `proof_of_work` function from the server.
+*X Change `valid_proof` to require *6* leading zeroes.
 * Add an endpoint called `last_proof` that returns the `proof` of the last block in the chain
 * Modify the `mine` endpoint to instead receive and validate or reject a new proof sent by a client.
 * Return a message indicating success or failure.  Remember, a valid proof should fail for all senders except the first.
@@ -18,8 +18,8 @@ Modify the server we created to:
 *Client Mining*
 Create a client application that will:
 * Request the latest proof from the `last_proof` endpoint on the server
-* Run the `proof_of_work` function until a valid proof is found, validating or rejecting each attempt
-* Print messages indicating that this has started and finished.  (Stretch: Add a timer)
+*X Run the `proof_of_work` function until a valid proof is found, validating or rejecting each attempt
+*X Print messages indicating that this has started and finished.  (Stretch: Add a timer)
 * Modify it to generate proofs with *6* leading zeroes.
 * When a valid proof is found, send it to the `mine` endpoint.  
 * Print a message indicating the success or failure response from the server
--- client_mining_p/miner.py
+++ client_mining_p/miner.py
@@ -4,8 +4,37 @@ import requests
 import sys
 
 
-# TODO: Implement functionality to search for a proof 
+# Implement functionality to search for a proof 
+def proof_of_work(last_proof):
+    """
+    Simple Proof of Work Algorithm
+    Find a number p such that hash(last_block_string, p) contains 6 leading
+    zeroes
+    """
+    print("Starting")
+    proof = 0
+    # for block 1, hash(1,p) = 000000x
+    while valid_proof(last_proof, proof) is False :
+        proof += 1
+    print("Sending to server...")    
+    return proof   
 
+def valid_proof(last_proof, proof):
+    """
+    Validates the Proof:  
+    Does hash(block_string, proof)
+    contain 6 leading zeroes?
+    """
+    #build string to hash
+    guess = f'{last_proof}{proof}'.encode()
+    #use hash function
+    guess_hash = hashlib.sha256(guess).hexdigest()
+    # check if 6 leading 0's
+    beg = guess_hash[0:6] # [:6]
+    if beg == "000000":
+        return True
+    else:
+        return False
 
 if __name__ == '__main__':
     # What node are we interacting with?
@@ -17,11 +46,19 @@ if __name__ == '__main__':
     coins_mined = 0
     # Run forever until interrupted
     while True:
-        # TODO: Get the last proof from the server and look for a new one
-        # TODO: When found, POST it to the server {"proof": new_proof}
-        # TODO: We're going to have to research how to do a POST in Python
-        # HINT: Research `requests` and remember we're sending our data as JSON
-        # TODO: If the server responds with 'New Block Forged'
-        # add 1 to the number of coins mined and print it.  Otherwise,
-        # print the message from the server.
-        pass
+        # Get the last proof from the server and look for a new one
+        # r = requests.get(url = node + "/last_proof")
+        #DO not need to use r.json(), but its good to use
+       
+
+        new_proof = proof_of_work(last_proof)
+        data = {'proof': new_proof}
+        # Assign our new_proof to 'proof'
+        proof_data = {'proof': new_proof}
+
+        # requests.post to /mine endpoint
+        r = requests.post(url = node + '/mine', data = data)
+
+        if r.message == 'New Block Forged':
+            coins_mined += 1
+            print('Coines mined: ', coins_mined)
