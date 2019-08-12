ad0502271b803c6aa4dca5d29dcdcd1aa54fa49a
Abigayle Franklin <abifanklin@gmail.com>
2019-08-12 12:22:12 -0500

    Route added for consensus

--- credit_for_mining_p/blockchain.py
+++ credit_for_mining_p/blockchain.py
@@ -307,7 +307,21 @@ def register_nodes():
     }
     return jsonify(response), 201
 
+@app.route('/nodes/resolve', methods=['GET'])
+def consensus():
+    replaced = blockchain.resolve_conflicts()
 
+    if replaces:
+        response = {
+            'message': 'Chain was replaced',
+            'new_chain': blockchain.chain
+        }
+    else:
+        response = {
+            'message': 'Chain is authoritative',
+            'chain': blockchain.chain
+        }
+    return jsonify(response), 200
 
 
 # Run the program on port 5000
