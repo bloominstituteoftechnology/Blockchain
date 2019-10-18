7b7d2e267b549bbc0f17aa0e2a5ae3b5c6a90928
briandoyle81 <briandoyle81@gmail.com>
2019-10-18 11:05:26 -0600

    update starter for client mining to include id

--- client_mining_p/miner.py
+++ client_mining_p/miner.py
@@ -30,34 +30,19 @@ if __name__ == '__main__':
     else:
         node = "http://localhost:5000"
 
-    coins_mined = 0
-
-    # Load or create ID
+    # Load ID
     f = open("my_id.txt", "r")
     id = f.read()
     print("ID is", id)
     f.close()
-    if len(id) == 0:
-        f = open("my_id.txt", "w")
-        # Generate a globally unique ID
-        id = str(uuid4()).replace('-', '')
-        print("Created new ID: " + id)
-        f.write(id)
-        f.close()
+
     # Run forever until interrupted
     while True:
-        # Get the last proof from the server
-        r = requests.get(url=node + "/last_block")
-        data = r.json()
-        new_proof = proof_of_work(data.get('last_block'))
-
-        post_data = {"proof": new_proof,
-                     "id": id}
-
-        r = requests.post(url=node + "/mine", json=post_data)
-        data = r.json()
-        if data.get('message') == 'New Block Forged':
-            coins_mined += 1
-            print("Total coins mined: " + str(coins_mined))
-        else:
-            print(data.get('message'))
+        # TODO: Get the last block from the server and look for a new proof
+        # TODO: When found, POST it to the server {"proof": new_proof, "id": id}
+        # TODO: We're going to have to research how to do a POST in Python
+        # HINT: Research `requests` and remember we're sending our data as JSON
+        # TODO: If the server responds with 'New Block Forged'
+        # add 1 to the number of coins mined and print it.  Otherwise,
+        # print the message from the server.
+        pass
