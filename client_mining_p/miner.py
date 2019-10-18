2e5d123bf054f10f05349893d40be563b73ce4e5
briandoyle81 <briandoyle81@gmail.com>
2019-10-18 10:55:50 -0600

    start bc 2.0

--- client_mining_p/miner.py
+++ client_mining_p/miner.py
@@ -2,26 +2,62 @@ import hashlib
 import requests
 
 import sys
+import json
+
+from uuid import uuid4
 
 
-# TODO: Implement functionality to search for a proof 
+def proof_of_work(block):
+    """
+    Find a number p such that hash(last_block, p) contains 6 leading
+    zeroes
+    """
+    pass
+
+
+def valid_proof(block_string, proof):
+    """
+    Validates the Proof:  Does hash(block_string, proof) contain 6
+    leading zeroes?
+    """
+    pass
 
 
 if __name__ == '__main__':
-    # What node are we interacting with?
+    # What port is the server on? IE `python3 miner.py 5001`
     if len(sys.argv) > 1:
-        node = sys.argv[1]
+        node = f'http://localhost:{int(sys.argv[1])}
     else:
         node = "http://localhost:5000"
 
     coins_mined = 0
+
+    # Load or create ID
+    f = open("my_id.txt", "r")
+    id = f.read()
+    print("ID is", id)
+    f.close()
+    if len(id) == 0:
+        f = open("my_id.txt", "w")
+        # Generate a globally unique ID
+        id = str(uuid4()).replace('-', '')
+        print("Created new ID: " + id)
+        f.write(id)
+        f.close()
     # Run forever until interrupted
     while True:
-        # TODO: Get the last proof from the server and look for a new one
-        # TODO: When found, POST it to the server {"proof": new_proof}
-        # TODO: We're going to have to research how to do a POST in Python
-        # HINT: Research `requests` and remember we're sending our data as JSON
-        # TODO: If the server responds with 'New Block Forged'
-        # add 1 to the number of coins mined and print it.  Otherwise,
-        # print the message from the server.
-        pass
+        # Get the last proof from the server
+        r = requests.get(url=node + "/last_block")
+        data = r.json()
+        new_proof = proof_of_work(data.get('last_block'))
+
+        post_data = {"proof": new_proof,
+                     "id": id}
+
+        r = requests.post(url=node + "/mine", json=post_data)
+        data = r.json()
+        if data.get('message') == 'New Block Forged':
+            coins_mined += 1
+            print("Total coins mined: " + str(coins_mined))
+        else:
+            print(data.get('message'))
