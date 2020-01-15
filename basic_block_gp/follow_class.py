235be38532ba2a616e9028d02169a16b617cfb45
johnschneider1 <johnlawschneider@gmail.com>
2020-01-15 17:26:18 -0600

    commit miner

--- basic_block_gp/follow_class.py
+++ basic_block_gp/follow_class.py
@@ -3,15 +3,12 @@ import json
 from time import time
 from uuid import uuid4
 from flask import Flask, jsonify, request
-
-
 class Blockchain(object):
     def __init__(self):
         self.chain = []
         self.current_transactions = []
         # Create the genesis block
         self.new_block(previous_hash=1, proof=100)
-
     def new_block(self, proof, previous_hash=None):
         """
         Create a new Block in the Blockchain
@@ -38,7 +35,6 @@ class Blockchain(object):
         self.chain.append(block)
         # Return the new block
         return block
-
     def hash(self, block):
         """
         Creates a SHA-256 hash of a Block
@@ -64,25 +60,22 @@ class Blockchain(object):
         # easier to work with and understand
         # TODO: Return the hashed block string in hexadecimal format
         return hex_hash
-
     @property
     def last_block(self):
         return self.chain[-1]
-
-    # def proof_of_work(self, block):
-    #     """
-    #     Simple Proof of Work Algorithm
-    #     Stringify the block and look for a proof.
-    #     Loop through possibilities, checking each one against `valid_proof`
-    #     in an effort to find a number that is a valid proof
-    #     :return: A valid proof for the provided block
-    #     """
-    #     block_string = json.dumps(block)
-    #     proof = 0
-    #     while self.valid_proof(block_string, proof) is False:
-    #         proof += 1
-    #     return proof
-
+    def proof_of_work(self, block):
+        """
+        Simple Proof of Work Algorithm
+        Stringify the block and look for a proof.
+        Loop through possibilities, checking each one against `valid_proof`
+        in an effort to find a number that is a valid proof
+        :return: A valid proof for the provided block
+        """
+        block_string = json.dumps(block)
+        proof = 0
+        while self.valid_proof(block_string, proof) is False:
+            proof += 1
+        return proof
     @staticmethod
     def valid_proof(block_string, proof):
         """
@@ -97,40 +90,24 @@ class Blockchain(object):
         """
         guess = f"{block_string}{proof}".encode()
         guess_hash = hashlib.sha256(guess).hexdigest()
-        return guess_hash[:6] == "000000"
-
-
+        return guess_hash[:3] == "000"
 # Instantiate our Node
 app = Flask(__name__)
 # Generate a globally unique address for this node
 node_identifier = str(uuid4()).replace('-', '')
 # Instantiate the Blockchain
 blockchain = Blockchain()
-
-
-@app.route('/mine', methods=['POST'])
+@app.route('/mine', methods=['GET'])
 def mine():
-    data = request.get_json()
-    required = ['proof', 'id']
-
-    if not all(k in data for k in required):
-        response = {'message': "Missing Values"}
-        return jsonify(response), 400
-
-    sub_proof = data.get('proof')
-
-    new_block = blockchain.last_block
-    last_block_string = json.dumps(new_block, sort_keys=True)
-
-    if blockchain.valid_proof(last_block_string, sub_proof):
-
-        response = {'message': "New Block Forged"}
-        return jsonify(response), 200
-    else:
-        response = {'message': "Proof Not Valid"}
-        return jsonify(response), 400
-
-
+    # Run the proof of work algorithm to get the next proof
+    proof = blockchain.proof_of_work(blockchain.last_block)
+    # Forge the new Block by adding it to the chain with the proof
+    previous_hash = blockchain.hash(blockchain.last_block)
+    block = blockchain.new_block(proof, previous_hash)
+    response = {
+        'new_block': block
+    }
+    return jsonify(response), 200
 @app.route('/chain', methods=['GET'])
 def full_chain():
     response = {
@@ -139,17 +116,6 @@ def full_chain():
         'chain': blockchain.chain
     }
     return jsonify(response), 200
-
-
-@app.route('/last_block', methods=['GET'])
-def last_block():
-    response = {
-        # TODO: Return the chain and its current length
-        'last_block': blockchain.last_block
-    }
-    return jsonify(response), 200
-
-
 # Run the program on port 5000
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000, debug=True)
--- client_mining_p/miner.py
+++ client_mining_p/miner.py
@@ -1,6 +1,7 @@
 import hashlib
 import requests
 
+
 import sys
 import json
 
@@ -13,7 +14,11 @@ def proof_of_work(block):
     in an effort to find a number that is a valid proof
     :return: A valid proof for the provided block
     """
-    pass
+    block_string = json.dumps(block, sort_keys=True)
+    proof = 0
+    while valid_proof(block_string, proof) is False:
+        proof += 1
+    return proof
 
 
 def valid_proof(block_string, proof):
@@ -27,7 +32,9 @@ def valid_proof(block_string, proof):
     correct number of leading zeroes.
     :return: True if the resulting hash is a valid proof, False otherwise
     """
-    pass
+    guess = f"{block_string}{proof}".encode()
+    guess_hash = hashlib.sha256(guess).hexdigest()
+    return guess_hash[:6] == "000000"
 
 
 if __name__ == '__main__':
@@ -43,6 +50,8 @@ if __name__ == '__main__':
     print("ID is", id)
     f.close()
 
+    coins_mined = 0
+
     # Run forever until interrupted
     while True:
         r = requests.get(url=node + "/last_block")
@@ -56,7 +65,7 @@ if __name__ == '__main__':
             break
 
         # TODO: Get the block from `data` and use it to look for a new proof
-        # new_proof = ???
+        new_proof = proof_of_work(data.get('last_block'))
 
         # When found, POST it to the server {"proof": new_proof, "id": id}
         post_data = {"proof": new_proof, "id": id}
@@ -67,4 +76,8 @@ if __name__ == '__main__':
         # TODO: If the server responds with a 'message' 'New Block Forged'
         # add 1 to the number of coins mined and print it.  Otherwise,
         # print the message from the server.
-        pass
+        if data.get("message") == "New Block Forged":
+            coins_mined += 1
+            print(f"Coins Mined: {coins_mined}")
+        else:
+            print(data.get("message"))
