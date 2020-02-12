6edc1e7bdc485eb2e23e5b9ff18158a94336a9ef
System Administrator <jon.s.solari@gmail.com>
2020-02-12 16:00:26 -0500

    :art: progress
    progress

--- client_mining_p/miner.py
+++ client_mining_p/miner.py
@@ -6,28 +6,21 @@ import json
 
 
 def proof_of_work(block):
-    """
-    Simple Proof of Work Algorithm
-    Stringify the block and look for a proof.
-    Loop through possibilities, checking each one against `valid_proof`
-    in an effort to find a number that is a valid proof
-    :return: A valid proof for the provided block
-    """
-    pass
+    
+    block_string = json.dumps(block, sort_keys=True)
+    proof = 0
+    while self.valid_proof(block_string, proof) is False:
+        proof += 1
+
+    return proof
 
 
 def valid_proof(block_string, proof):
-    """
-    Validates the Proof:  Does hash(block_string, proof) contain 6
-    leading zeroes?  Return true if the proof is valid
-    :param block_string: <string> The stringified block to use to
-    check in combination with `proof`
-    :param proof: <int?> The value that when combined with the
-    stringified previous block results in a hash that has the
-    correct number of leading zeroes.
-    :return: True if the resulting hash is a valid proof, False otherwise
-    """
-    pass
+    guess = f'{block_string}{proof}'.encode()
+    guess_hash = hashlib.sha256(guess).hexdigest()
+
+    if guess_hash[:6] == "000000":
+        return True
 
 
 if __name__ == '__main__':
@@ -45,7 +38,7 @@ if __name__ == '__main__':
 
     # Run forever until interrupted
     while True:
-        r = requests.get(url=node + "/last_block")
+        r = requests.get(url=node + "/last")
         # Handle non-json response
         try:
             data = r.json()
@@ -56,7 +49,7 @@ if __name__ == '__main__':
             break
 
         # TODO: Get the block from `data` and use it to look for a new proof
-        # new_proof = ???
+        new_proof = data
 
         # When found, POST it to the server {"proof": new_proof, "id": id}
         post_data = {"proof": new_proof, "id": id}
