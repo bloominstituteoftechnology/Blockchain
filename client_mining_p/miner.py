edd1ef646f9746835b55729a4e196fb3de049788
SamH3pn3r <samuelhepner03@gmail.com>
2020-02-12 13:53:25 -0800

    Did some blockchaining

--- client_mining_p/miner.py
+++ client_mining_p/miner.py
@@ -13,7 +13,11 @@ def proof_of_work(block):
     in an effort to find a number that is a valid proof
     :return: A valid proof for the provided block
     """
-    pass
+    block_string = json.dumps(block, sort_keys=True)
+    proof = 0
+    while valid_proof(block_string, proof) is False:
+        proof += 1
+    return proof
 
 
 def valid_proof(block_string, proof):
@@ -27,15 +31,19 @@ def valid_proof(block_string, proof):
     correct number of leading zeroes.
     :return: True if the resulting hash is a valid proof, False otherwise
     """
-    pass
+    guess = f'{block_string}{proof}'.encode()
+    guess_hash = hashlib.sha256(guess).hexdigest()
+        
+    return guess_hash[:6] == "000000"
 
 
 if __name__ == '__main__':
+    coins = 0
     # What is the server address? IE `python3 miner.py https://server.com/api/`
     if len(sys.argv) > 1:
         node = sys.argv[1]
     else:
-        node = "http://localhost:5000"
+        node = "http://127.0.0.1:5000"
 
     # Load ID
     f = open("my_id.txt", "r")
@@ -56,7 +64,7 @@ if __name__ == '__main__':
             break
 
         # TODO: Get the block from `data` and use it to look for a new proof
-        # new_proof = ???
+        new_proof = proof_of_work(data['last_block'])
 
         # When found, POST it to the server {"proof": new_proof, "id": id}
         post_data = {"proof": new_proof, "id": id}
@@ -64,7 +72,11 @@ if __name__ == '__main__':
         r = requests.post(url=node + "/mine", json=post_data)
         data = r.json()
 
-        # TODO: If the server responds with a 'message' 'New Block Forged'
+        # TODO: If the server responds with a 'message' 'success'
         # add 1 to the number of coins mined and print it.  Otherwise,
         # print the message from the server.
-        pass
+        if (data['message'] == 'success'):
+            coins += 1
+            print(f'You have mined {coins} coins.')
+        else:
+            print(data['message'])
