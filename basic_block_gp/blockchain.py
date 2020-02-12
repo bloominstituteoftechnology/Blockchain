1161a793cd9c84638adf483fa5fad1652022d48d
System Administrator <jon.s.solari@gmail.com>
2020-02-12 14:59:37 -0500

    :art: foundations
    foundations

--- basic_block_gp/blockchain.py
+++ basic_block_gp/blockchain.py
@@ -113,7 +113,7 @@ class Blockchain(object):
         guess = f'{block_string}{proof}'.encode()
         guess_hash = hashlib.sha256(guess).hexdigest()
 
-        return guess_hash[:6] == "000000"
+        return guess_hash[:3] == "000"
 
 
 # Instantiate our Node
