f64361c5eee6f8c0710702e349234c601b5bea33
sandramkimball <53502963+sandramkimball@users.noreply.github.com>
2020-03-11 13:31:20 -0700

    completed miner.py but is prob incorrect

--- client_mining_p/miner.py
+++ client_mining_p/miner.py
@@ -6,28 +6,18 @@ import json
 
 
 def proof_of_work(block):
-    """
-    Simple Proof of Work Algorithm
-    Stringify the block and look for a proof.
-    Loop through possibilities, checking each one against `valid_proof`
-    in an effort to find a number that is a valid proof
-    :return: A valid proof for the provided block
-    """
-    pass
+    block_string = json.dumps(block, sort_keys=True)
+    proof = 0
+    while self.valid_proof(block_string, proof): #until num proof is found
+        proof += 1
+    return proof
 
 
 def valid_proof(block_string, proof):
-    """
-    Validates the Proof:  Does hash(block_string, proof) contain 6
-    leading zeroes?  Return true if the proof is valid
-    :param block_string: <string> The stringified block to use to
-    check in combination with `proof`
-    :param proof: <int?> The value that when combined with the
-    stringified previous block results in a hash that has the
-    correct number of leading zeroes.
-    :return: True if the resulting hash is a valid proof, False otherwise
-    """
-    pass
+    guess = f'{block_string}{proof}'
+    guess_hash = hashlib.sha256(guess).hexdigest()
+
+    return guess_hash[:6] =='000000'
 
 
 if __name__ == '__main__':
@@ -37,16 +27,15 @@ if __name__ == '__main__':
     else:
         node = "http://localhost:5000"
 
-    # Load ID
+    # Load ID -- ID FOR WHAT? WHAT HAS AN ID???
     f = open("my_id.txt", "r")
     id = f.read()
     print("ID is", id)
     f.close()
 
-    # Run forever until interrupted
+    # Run until interrupted, and handle non-json response:
     while True:
         r = requests.get(url=node + "/last_block")
-        # Handle non-json response
         try:
             data = r.json()
         except ValueError:
@@ -55,8 +44,9 @@ if __name__ == '__main__':
             print(r)
             break
 
-        # TODO: Get the block from `data` and use it to look for a new proof
-        # new_proof = ???
+        # TODO: Get block from `data` and use it to look for a new proof
+        new_proof = data.blockchain.proof_of_work(blockchain.last_block) #gets next proof: same thing??
+
 
         # When found, POST it to the server {"proof": new_proof, "id": id}
         post_data = {"proof": new_proof, "id": id}
@@ -67,4 +57,11 @@ if __name__ == '__main__':
         # TODO: If the server responds with a 'message' 'New Block Forged'
         # add 1 to the number of coins mined and print it.  Otherwise,
         # print the message from the server.
-        pass
+        coins = 0
+        if message = 'New Block Forged':
+            coins += 1
+            response = {'coins': coins}
+            return jsonify(response)
+        else: 
+            return jsonify(message)
+
