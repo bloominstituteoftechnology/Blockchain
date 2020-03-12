352e4d91170254347793f2abb53ff83e170c325a
gelinas <jlgelinas@gmail.com>
2020-03-12 10:39:10 -0400

    initialize blockchain app

--- basic_block_gp/blockchain.py
+++ basic_block_gp/blockchain.py
@@ -31,13 +31,19 @@ class Blockchain(object):
         """
 
         block = {
-            # TODO
+            'index': len(self.chain) + 1,
+            'timestamp': time(),
+            'transactions': self.current_transactions,
+            'proof': proof,
+            'previous_hash': previous_hash or self.hash(self.last_block)
         }
 
         # Reset the current list of transactions
+        self.current_transactions = []
         # Append the chain to the block
+        self.chain.append(block)
         # Return the new block
-        pass
+        return block
 
     def hash(self, block):
         """
@@ -56,8 +62,10 @@ class Blockchain(object):
         # or we'll have inconsistent hashes
 
         # TODO: Create the block_string
+        string_block = json.dumps(block, sort_keys=True)
 
         # TODO: Hash this string using sha256
+        raw_hash = hashlib.sha256(string_block.encode())
 
         # By itself, the sha256 function returns the hash in a raw string
         # that will likely include escaped characters.
@@ -66,7 +74,8 @@ class Blockchain(object):
         # easier to work with and understand
 
         # TODO: Return the hashed block string in hexadecimal format
-        pass
+        hex_hash = raw_hash.hexdigest()
+        return hex_hash
 
     @property
     def last_block(self):
@@ -128,6 +137,8 @@ def mine():
 def full_chain():
     response = {
         # TODO: Return the chain and its current length
+        'chain': blockchain.chain,
+        'length': len(blockchain.chain)
     }
     return jsonify(response), 200
 
